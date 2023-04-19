5b9855d5cd4fb908306aad58858bcd25781e8b06
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-20 07:24:44 +0900

    Data Layer - Models & Datasources

---
+++ lib/0_data/datasources/advice_remote_datasource.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:advicer/0_data/models/advice_model.dart';
+import 'package:http/http.dart' as http;
+
+abstract class AdviceRemoteDatasource {
+  Future<AdviceModel> getRandomAdviceFromApi();
+}
+
+class AdviceRemoteDatasourceImpl implements AdviceRemoteDatasource {
+  final client = http.Client();
+  @override
+  Future<AdviceModel> getRandomAdviceFromApi() async {
+    final response = await client.get(
+      Uri.parse(
+        'https://api.flutter-community.com/api/v1/advice',
+      ),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    );
+    final responseBody = json.decode(response.body);
+    return AdviceModel.fromJson(responseBody);
+  }
+}
--- lib/1_domain/usecases/advice_usecases.dart
+++ lib/1_domain/usecases/advice_usecases.dart
@@ -1,20 +1,11 @@
+import 'package:advicer/0_data/repositories/advice_repo_impl.dart';
 import 'package:advicer/1_domain/entities/advice_entity.dart';
 import 'package:advicer/1_domain/failures/failures.dart';
 import 'package:dartz/dartz.dart';
 
 class AdviceUseCases {
+  final adviceRepo = AdviceRepoImpl();
   Future<Either<Failure, AdviceEntity>> getAdvice() async {
-    await Future.delayed(
-      const Duration(
-        seconds: 3,
-      ),
-      () {},
-    );
-    return right(
-      const AdviceEntity(
-        advice: 'advice to test',
-        id: 1,
-      ),
-    );
+    return adviceRepo.getAdviceFromDatasource();
   }
 }
