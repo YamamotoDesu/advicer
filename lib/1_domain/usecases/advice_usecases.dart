a748e139081f797c82f07863e85147da5c4ca2af
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-19 18:08:57 +0900

    Domain Layer - Failures & Dartz

---
+++ lib/1_domain/usecases/advice_usecases.dart
@@ -0,0 +1,20 @@
+import 'package:advicer/1_domain/entities/advice_entity.dart';
+import 'package:advicer/1_domain/failures/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class AdviceUseCases {
+  Future<Either<Failure, AdviceEntity>> getAdvice() async {
+    await Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+      () {},
+    );
+    return right(
+      const AdviceEntity(
+        advice: 'advice to test',
+        id: 1,
+      ),
+    );
+  }
+}
--- lib/2_application/pages/advice/cubit/advicer_cubit.dart
+++ lib/2_application/pages/advice/cubit/advicer_cubit.dart
@@ -1,27 +1,36 @@
-import 'package:bloc/bloc.dart';
+import 'package:advicer/1_domain/failures/failures.dart';
+import 'package:advicer/1_domain/usecases/advice_usecases.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'advicer_state.dart';
 
+const generalFailureMessage = 'Oops, Unknown Error, please try again!';
+const serverFailureMessage = 'Oops, API Error, please try again!';
+const cacheFailureMessage = 'Oops, Cache Error, please try again!';
+
 class AdvicerCubit extends Cubit<AdvicerQubitState> {
   AdvicerCubit() : super(AdvicerInitial());
+  final AdviceUseCases adviceUseCases = AdviceUseCases();
 
   void adviceRequested() async {
     emit(AdvicerStateLoading());
 
-    debugPrint('fake get advice triggered');
-    await Future.delayed(
-      const Duration(
-        seconds: 3,
-      ),
-      () {},
+    final failureOrAdvice = await adviceUseCases.getAdvice();
+    failureOrAdvice.fold(
+      (failure) => emit(AdvicerStateError(message: _mapFailureToMesssage(failure))),
+      (advice) => emit(AdvicerStateLoaded(advice: advice.advice)),
     );
-    debugPrint('got advice');
-    emit(
-      const AdvicerStateLoaded(
-        advice: 'fake advice to test block',
-      ),
-    );
+  }
+
+  String _mapFailureToMesssage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return generalFailureMessage;
+    }
   }
 }
