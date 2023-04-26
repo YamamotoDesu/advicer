6a33e46bc4b1ce9c6e1290a7564ff93d8fcc642d
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2023-04-26 09:52:29 +0900

    add IT

--- lib/2_application/pages/advice/widgets/custom_button.dart
+++ lib/2_application/pages/advice/widgets/custom_button.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
+  final Function()? onTap;
+
+ const CustomButton({this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
